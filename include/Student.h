bcabdfc19668565f1fad42009634eb7a1afe680e
Oscar David Arbeláez <odarbelaeze@gmail.com>
2014-10-01 21:19:13 -0500

    Implementing a couple of properties in the Student class.

--- include/Student.h
+++ include/Student.h
@@ -1,11 +1,22 @@
 #ifndef STUDENT_H_
 #define STUDENT_H_
 
+#include <string>
+
 class Student
 {
 public:
     Student();
     ~Student();
+
+    std::string getName() const;
+    void setName(std::string);
+    std::string getId() const;
+    void setId(std::string);
+
+private:
+    std::string name;
+    std::string id;
 
 };
 
--- src/Student.cc
+++ src/Student.cc
@@ -10,3 +10,27 @@ Student::~Student()
 {
 
 }
+
+
+std::string Student::getName() const
+{
+    return this -> name;
+}
+
+
+void Student::setName(std::string name)
+{
+    this -> name = name;
+}
+
+
+std::string Student::getId() const
+{
+    return this -> id;
+}
+
+
+void Student::setId(std::string id)
+{
+    this -> id = id;
+}
